0a6fccfe35ba07ea76d9fcbfd381da2bb5ef6d02
Abhi kumar <116167998+abhik2022@users.noreply.github.com>
2024-01-27 23:08:11 +0530

    Delete my-app directory

--- my-app/dom/script.js
+++
@@ -1,8 +0,0 @@
-const btn=document.querySelector('.btn');
-const back=document.querySelector('.main');
-console.log(back)
-btn.addEventListener('click',()=>{
-    console.log("hello");
-    back.innerHTML="DIV CHANGED";
-    back.style='background-color:pink'
-})
--- my-app/module1.mjs
+++
@@ -1,7 +0,0 @@
-import dz, {a, c, d} from "./module2.mjs"
-
-// use curly braces when u want something that is not exported by default from other module...
-
-console.log(dz);
-console.log(a);
-console.log(d);
